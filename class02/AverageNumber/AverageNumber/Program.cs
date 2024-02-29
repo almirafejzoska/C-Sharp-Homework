b8cdce276e7e7bd8ba82bb8ed651ab590f373050
almirafejzoska <almirafejzoska@protonmail.com>
2024-02-29 02:08:20 +0100

    second homework

---
+++ class02/AverageNumber/AverageNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace AverageNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write your first number");
+            int firstNum;
+            bool firstNumber = int.TryParse(Console.ReadLine(), out firstNum);
+
+            Console.WriteLine("Write your second number");
+            int secondNum;
+            bool secondNumber = int.TryParse(Console.ReadLine(), out secondNum);
+
+            Console.WriteLine("Write your third number");
+            int thirdNum;
+            bool thirdNumber = int.TryParse(Console.ReadLine(), out thirdNum);
+
+            Console.WriteLine("Write your fourth number");
+            int forthNum;
+            bool forthNumber = int.TryParse(Console.ReadLine(), out forthNum);
+
+            int averageNum = (firstNum + secondNum + thirdNum + forthNum) / 4;
+            Console.WriteLine($"The average of {firstNum}, {secondNum}, {thirdNum} and {forthNum} is:{averageNum}");
+        }
+    }
+}
---
+++ class02/SwapNumbers/SwapNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace SwapNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum;
+            int secondNum;
+            int forSwap;
+
+            Console.WriteLine("Input the first number");
+            bool firstNumber = int.TryParse(Console.ReadLine(), out firstNum);
+
+            Console.WriteLine("Input the second number");
+            bool secondNumber = int.TryParse(Console.ReadLine(), out secondNum);
+
+            forSwap = firstNum;
+            firstNum = secondNum;
+            secondNum = forSwap;
+
+
+            Console.WriteLine("After Swapping:");
+            Console.WriteLine($"First Number:{firstNum}");
+            Console.WriteLine($"Second Number: {secondNum}");
+        }
+    }
+}
