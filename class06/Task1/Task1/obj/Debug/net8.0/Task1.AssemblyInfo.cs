98197cd098aac23421b585442cfa75842edd50a4
almirafejzoska <almirafejzoska@protonmail.com>
2024-03-20 16:31:25 +0100

    homework

--- class06/Task1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ class06/Task1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9698712e2c2efb5434dc1fcc2086dd36f7ba5426")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
